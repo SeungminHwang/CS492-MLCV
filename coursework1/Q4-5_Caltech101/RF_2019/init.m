d42990b5d06e51ae3166d63a135fdb0bf77a6fe5
Seungmin Hwang <hwangtmdals@kaist.ac.kr>
2020-10-19 03:21:02 +0900

    fix: module version

--- coursework1/Q4-5_Caltech101/RF_2019/init.m
+++ coursework1/Q4-5_Caltech101/RF_2019/init.m
@@ -9,9 +9,8 @@ addpath('./external');
 addpath('./external/libsvm-3.18/matlab');
 
 % initialise external libraries
-run('external/vlfeat-0.9.18/toolbox/vl_setup.m'); % vlfeat library
+run('external/vlfeat-0.9.21/toolbox/vl_setup.m'); % vlfeat library
 cd('external/libsvm-3.18/matlab'); % libsvm library
 run('make');
 cd('../../..');
 
-% tested on Ubuntu 12.04, 64-bit, Intel® Core�?i7-3820 CPU @ 3.60GHz × 8 
